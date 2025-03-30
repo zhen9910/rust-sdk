ac1b8c6ddb12df31ed6dbf5d4eb40102ebef594c
Jefry Dewangga <jefrydco@gmail.com>
2025-03-30 16:22:14 +0000

    test: add test with js server (#65)

--- crates/rmcp/tests/test_with_js.rs
+++ crates/rmcp/tests/test_with_js.rs
@@ -1,4 +1,7 @@
-use rmcp::transport::sse_server::SseServer;
+use rmcp::{
+    ServiceExt,
+    transport::{SseServer, TokioChildProcess},
+};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 mod common;
 use common::calculator::Calculator;
@@ -32,5 +35,34 @@ async fn test_with_js_client() -> anyhow::Result<()> {
         .await?;
     assert!(exit_status.success());
     ct.cancel();
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_with_js_server() -> anyhow::Result<()> {
+    let _ = tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "debug".to_string().into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .try_init();
+    tokio::process::Command::new("npm")
+        .arg("install")
+        .current_dir("tests/test_with_js")
+        .spawn()?
+        .wait()
+        .await?;
+    let transport = TokioChildProcess::new(
+        tokio::process::Command::new("node").arg("tests/test_with_js/server.js"),
+    )?;
+
+    let client = ().serve(transport).await?;
+    let resources = client.list_all_resources().await?;
+    tracing::info!("{:#?}", resources);
+    let tools = client.list_all_tools().await?;
+    tracing::info!("{:#?}", tools);
+
+    client.cancel().await?;
     Ok(())
 }
