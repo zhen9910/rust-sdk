01eedb77704fd32d66dea455431b29a03923bdf4
jokemanfire <hu.dingyang@zte.com.cn>
2025-05-16 14:40:28 +0800

    fix(schemar): use self-defined settings (#180)
    Ignores fields that are not defined in the spec file.
    
    Signed-off-by: jokemanfire <hu.dingyang@zte.com.cn>

--- crates/rmcp/src/handler/server/tool.rs
+++ crates/rmcp/src/handler/server/tool.rs
@@ -14,7 +14,13 @@ use crate::{
 };
 /// A shortcut for generating a JSON schema for a type.
 pub fn schema_for_type<T: JsonSchema>() -> JsonObject {
-    let settings = schemars::r#gen::SchemaSettings::openapi3();
+    let mut settings = schemars::r#gen::SchemaSettings::default();
+    settings.option_nullable = true;
+    settings.option_add_null_type = false;
+    settings.definitions_path = "#/components/schemas/".to_owned();
+    settings.meta_schema = None;
+    settings.visitors = Vec::default();
+    settings.inline_subschemas = false;
     let generator = settings.into_generator();
     let schema = generator.into_root_schema_for::<T>();
     let object = serde_json::to_value(schema).expect("failed to serialize schema");
