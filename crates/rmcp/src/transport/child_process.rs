db03f63e76b5b32f65d34a1bd08ae56dab595f60
Lucas Nogueira <118899497+lucasfernog-crabnebula@users.noreply.github.com>
2025-06-07 11:59:15 -0300

    allow using a TokioCommandWrap for TokioChildProcess::new closes #243 (#245)
    * allow using a TokioCommandWrap for TokioChildProcess::new closes #243
    
    TokioCommandWrap implements From<tokio::process::Command> so this is not a breaking change, but also downstream users have more control over flags such as KillOnDrop and CreationFlags
    
    see https://docs.rs/process-wrap/latest/process_wrap/#killondrop-and-creationflags

--- crates/rmcp/src/transport/child_process.rs
+++ crates/rmcp/src/transport/child_process.rs
@@ -59,11 +59,12 @@ impl AsyncRead for TokioChildProcessOut {
 }
 
 impl TokioChildProcess {
-    pub fn new(mut command: tokio::process::Command) -> std::io::Result<Self> {
-        command
+    pub fn new(command: impl Into<TokioCommandWrap>) -> std::io::Result<Self> {
+        let mut command_wrap = command.into();
+        command_wrap
+            .command_mut()
             .stdin(std::process::Stdio::piped())
             .stdout(std::process::Stdio::piped());
-        let mut command_wrap = TokioCommandWrap::from(command);
         #[cfg(unix)]
         command_wrap.wrap(process_wrap::tokio::ProcessGroup::leader());
         #[cfg(windows)]
